29a3da25a715fddfbe8d5e95b7bf1cd61d15efb9
Javier López Úbeda <jlopezcur@gmail.com>
2020-06-15 16:53:35 +0200

    feat: change the title of the page for seo

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   pathPrefix: '/expo',
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Creatividad Confinada',
     description:
       'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
     author: '@gatsbyjs',
