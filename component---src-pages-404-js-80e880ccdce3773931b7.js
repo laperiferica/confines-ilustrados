4f9e424e7ba081ffe84b14611ceed3c811b39d7a
jlopezcur <jlopezcur@users.noreply.github.com>
2020-06-15 17:34:08 +0000

    deploy: a13089412f38b0ffae55dfff892c26f579fd3f4c

---
+++ component---src-pages-404-js-80e880ccdce3773931b7.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{w2l6:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),l=a.n(n),u=a("hYuR");t.default=function(e){var t=e.pageContext;return l.a.createElement(l.a.Fragment,null,l.a.createElement(u.a,{title:"404",lang:t.language}),l.a.createElement("h1",null,"NOT FOUND"),l.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-80e880ccdce3773931b7.js.map
