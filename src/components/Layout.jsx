33d56e372196706f07fdb1817d6c7905e6c56abc
Javier López Úbeda <jlopezcur@gmail.com>
2020-06-22 22:48:08 +0200

    fix: added margin to footer

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 import Go2Top from './Go2Top';
 import GoBack from './GoBack';
 
 import '../styles/layout.css';
 
+const StyledLayout = styled.div`
+  main {
+    margin-bottom: 4rem;
+  }
+`;
+
 const Layout = ({ children, pageContext }) => (
-  <>
+  <StyledLayout>
     <main>{children}</main>
     {pageContext.slug && <GoBack lang={pageContext.language} />}
     <Go2Top />
-  </>
+  </StyledLayout>
 );
 
 Layout.propTypes = {
