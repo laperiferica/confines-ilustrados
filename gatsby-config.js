2c4a31d7d0dbb6b28be35f45fc39193214e6e153
Javier López Úbeda <jlopezcur@gmail.com>
2020-06-22 18:21:18 +0200

    feat: added favicon
    close #18

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,13 +53,13 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'confines ilustrados',
+        short_name: 'confines ilustrados',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        background_color: '#000000',
+        theme_color: '#FFFFFF',
+        display: 'standalone',
+        icon: 'src/images/favicon.png', // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
